ae6c978fa8490c87fce7d65f96ffe2ce3c0c0989
Stijn Tallon <stijn_tallon@msn.com>
2016-03-26 18:32:19 +0100

    ColorFCollection

---
+++ src/Nexus/Design/ColorFCollectionConverter.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.ComponentModel.Design.Serialization;
+using System.Reflection;
+using Nexus.Graphics.Colors;
+
+namespace Nexus.Design
+{
+	public sealed class ColorFCollectionConverter : TypeConverter
+	{
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return ((sourceType == typeof(string)) || base.CanConvertFrom(context, sourceType));
+		}
+
+		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			if (value == null)
+				throw GetConvertFromException(value);
+
+			string source = value as string;
+			if (source != null)
+				return ColorFCollection.Parse(source);
+			return base.ConvertFrom(context, culture, value);
+		}
+
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+			if (destinationType == typeof(InstanceDescriptor))
+				return true;
+
+			return ((destinationType == typeof(string)) || base.CanConvertTo(context, destinationType));
+		}
+
+		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		{
+			if ((destinationType != null) && (value is ColorFCollection))
+			{
+				ColorFCollection vectords = (ColorFCollection) value;
+				if (destinationType == typeof(InstanceDescriptor))
+				{
+					ConstructorInfo ci = typeof(ColorFCollection).GetConstructor(new Type[] { typeof(ColorF[]) });
+					return new InstanceDescriptor(ci, new object[] { vectords.ToArray() });
+				}
+				else if (destinationType == typeof(string))
+					return vectords.ConvertToString(null, culture);
+			}
+			return base.ConvertTo(context, culture, value, destinationType);
+		}
+	}
+}
---
+++ src/Nexus/Graphics/Colors/ColorFCollection.cs
@@ -0,0 +1,53 @@
+﻿using Nexus.Design;
+using Nexus.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Nexus.Graphics.Colors
+{
+    [TypeConverter(typeof(ColorFCollectionConverter))]
+    public class ColorFCollection : List<ColorF>
+    {
+        public ColorFCollection()
+        {
+
+        }
+
+        public ColorFCollection(IEnumerable<ColorF> collection)
+            : base(collection)
+        {
+
+        }
+
+        internal string ConvertToString(string format, IFormatProvider provider)
+        {
+            if (this.Count == 0)
+                return string.Empty;
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < this.Count; i++)
+            {
+                builder.AppendFormat(provider, "{0:" + format + "}", new object[] { this[i] });
+                if (i != (this.Count - 1))
+                    builder.Append(" ");
+            }
+            return builder.ToString();
+        }
+
+        public static ColorFCollection Parse(string source)
+        {
+            IFormatProvider cultureInfo = CultureInfo.InvariantCulture;
+            TokenizerHelper helper = new TokenizerHelper(source, cultureInfo);
+            ColorFCollection colors = new ColorFCollection();
+            while (helper.NextToken())
+            {
+                ColorF color = new ColorF(Convert.ToSingle(helper.GetCurrentToken(), cultureInfo), Convert.ToSingle(helper.NextTokenRequired(), cultureInfo), Convert.ToSingle(helper.NextTokenRequired(), cultureInfo), Convert.ToSingle(helper.NextTokenRequired(), cultureInfo));
+                colors.Add(color);
+            }
+            return colors;
+        }
+    }
+}
